3643448ee60aa0a02a2570417ee4f66a120897ce
Govind-S-B <b.s.dnivog@gmail.com>
2023-05-10 14:06:30 +0530

    basic app components

---
+++ lib/scanner_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class scannerPage extends StatelessWidget {
+  const scannerPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Text("Hi"),
+        ],
+      ),
+    );
+  }
+}
