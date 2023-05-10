16c2951dad262dbea44d1b86d535277f0814b6f8
Govind-S-B <b.s.dnivog@gmail.com>
2023-05-10 17:41:37 +0530

    added qr scanner , decode qr , pass it to a blank second screen

---
+++ lib/cat_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CatDisplayPage extends StatelessWidget {
+  final String colorCat;
+  const CatDisplayPage({super.key,required this.colorCat});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/scanner_page.dart
+++ lib/scanner_page.dart
@@ -1,14 +1,29 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class scannerPage extends StatelessWidget {
-  const scannerPage({super.key});
+import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+import 'cat_page.dart';
+
+class ScannerPage extends StatelessWidget {
+  const ScannerPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          Text("Hi"),
+          MobileScanner(onDetect: (capture) {
+            for(var barcode in capture.barcodes){
+              try{
+                Map decodedQr = jsonDecode(barcode.toString());
+                Navigator.push(context, MaterialPageRoute(builder: (context) => CatDisplayPage(colorCat: decodedQr["color"],),));
+              }
+              catch(r){
+                // will add stuff
+              }
+            }
+          },)
         ],
       ),
     );
